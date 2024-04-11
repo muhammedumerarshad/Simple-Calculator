a0f54274f72d0c8c51e71187cdbf3810523bbad4
muhammedumerarshad <umerarshad1706@gmail.com>
2024-04-11 21:17:49 +0500

    push my calculator

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquiror from "inquirer";
+const answer = await inquiror.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform operations",
+        type: "list",
+        name: "operators",
+        choices: ["Addition", "Multiplication", "Subtraction", "Division", "None"],
+    },
+]);
+//conditionals statements
+if (answer.operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operators === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log('Your operation cannot be performed');
+}
